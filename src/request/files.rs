ad469fa370e424ed587e39da85bb490cb64e19a6
Elttob <dan.bottlefact@gmail.com>
2022-08-27 13:38:36 +0100

    Fixed file url

--- src/request/files.rs
+++ src/request/files.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use crate::{data, json};
 
 pub async fn get(id: &str) -> Result<data::files::Files> {
-    let url = format!("https://api.polyhaven.com/info/{}", id);
+    let url = format!("https://api.polyhaven.com/files/{}", id);
     let resp = reqwest::get(url).await?.json::<json::files::Files>().await?;
     Ok(resp.into())
 }
